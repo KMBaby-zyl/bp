fec04153a69a9f56838bc7aeb2791796ba98bb9b
KMBaby-zyl <zhangyili911229@gmail.com>
2021-04-20 17:38:14 +0800

    fix bin

--- bin/bp.js
+++ bin/bp.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
+const yParser = require('yargs-parser');
+const args = yParser(process.argv.slice(2));
+const cwd = process.cwd();
+const path = require('path');
+const pkg = require(path.resolve(cwd, 'package.json'));
 const bp = require('../index');
 
-bp.runLerna();
+
+bp.run(pkg, args);
